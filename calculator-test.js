365662da0511c560087dacbed6c904040a6c3d44
toasty <daniel.m84@outlook.com>
2022-11-16 15:32:43 -0500

    clean up

--- calculator-test.js
+++ calculator-test.js
@@ -1,7 +1,9 @@
+describe('calculateMonthlyPayment test', function() {
+  it('should calculate the monthly rate correctly', function () {
+    // ...
+  });
+})
 
-it('should calculate the monthly rate correctly', function () {
-  // ...
-});
 
 
 it("should return a result with 2 decimal places", function() {
