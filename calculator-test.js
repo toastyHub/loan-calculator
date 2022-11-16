fc8e7cca7610f3888142af010eadda65a65d8a7b
toasty <daniel.m84@outlook.com>
2022-11-16 15:56:47 -0500

    Passes tests

--- calculator-test.js
+++ calculator-test.js
@@ -1,13 +1,22 @@
-describe('calculateMonthlyPayment test', function() {
+describe('calculateMonthlyPayment Test', function() {
   it('should calculate the monthly rate correctly', function () {
-    // ...
+    const values = {
+      amount: 20000,
+      years: 10,
+      rate: 2.5
+    };
+    expect(calculateMonthlyPayment(values)).toEqual('188.54')
   });
 })
 
+describe('calculateMonthlyPayment Test', function() {
+  it('should return a result with 2 decimal places', function () {
+    const values = {
+      amount: 25000,
+      years: 15,
+      rate: 5.5
+    };
+    expect(calculateMonthlyPayment(values)).toEqual('204.27')
+  });
+})
 
-
-it("should return a result with 2 decimal places", function() {
-  // ..
-});
-
-/// etc
